1356b01a57341f08b72fb6ac52c34ab34901de21
MineMine <gaoyuan_flying@163.com>
2025-07-15 14:15:23 +0800

    feat:上传项目

---
+++ README.md
@@ -0,0 +1,11 @@
+# gy-prettier
+
+基于prettier的格式配置文件
+
+## 使用方式
+1. package.json 中的 type 为 module 时
+新建 .prettierrc.js 内容如下
+```javascript
+import GyPrettier from 'gy-prettier';
+export default GyPrettier;
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // 每行最大字符数
+  printWidth: 80,
+  // 缩进空格数
+  tabWidth: 2,
+  // 使用空格缩进
+  useTabs: false,
+  // 句末分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 对象属性引号
+  quoteProps: "as-needed",
+  // 箭头函数参数括号
+  arrowParens: "always",
+  // 尾随逗号
+  trailingComma: "all",
+  // JSX 单引号
+  jsxSingleQuote: false,
+  // HTML 空格敏感度
+  htmlWhitespaceSensitivity: "css",
+  // Vue 文件脚本和样式标签缩进
+  vueIndentScriptAndStyle: false,
+  // 换行符
+  endOfLine: "lf",
+  //在对象前后添加空格-eg: { foo: bar }
+  bracketSpacing: true,
+};
